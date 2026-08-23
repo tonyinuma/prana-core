cc86f071665f03615e1477c161d098e3f7c13599
Tony ID <tony.inuma@gmail.com>
2026-08-23 12:53:11 -0500

    add: prettier config

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,20 @@
 const eslint = require('@eslint/js');
 const { defineConfig, globalIgnores } = require('eslint/config');
+const prettier = require('eslint-config-prettier');
 const globals = require('globals');
 const tseslint = require('typescript-eslint');
 
 module.exports = defineConfig(
-  globalIgnores(['node_modules/**', 'dist/**', 'coverage/**', '.serverless/**']),
-  eslint.configs.recommended,
-  {
-    files: ['**/*.ts'],
-    extends: [tseslint.configs.recommended],
-  },
-  {
-    languageOptions: {
-      globals: globals.node,
+    globalIgnores(['node_modules/**', 'dist/**', 'coverage/**', '.serverless/**']),
+    eslint.configs.recommended,
+    {
+        files: ['**/*.ts'],
+        extends: [tseslint.configs.recommended],
     },
-  },
+    {
+        languageOptions: {
+            globals: globals.node,
+        },
+    },
+    prettier,
 );
